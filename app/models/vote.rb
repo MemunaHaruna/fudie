2241b1b5b6468d6b69b90d297641485799c15b89
memuna haruna <memuna.haruna@andela.com>
2019-03-18 01:52:57 +0100

    add model tests

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,5 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   validates_uniqueness_of :post_id, scope: :user_id
-
   enum vote_type: %w[upvote downvote]
 end
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Category, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+
+  it { should have_many(:posts) }
+  it { should have_many(:users) }
+end
